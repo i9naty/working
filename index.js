721e913e56e37dbd6443437c683982d821a95a8b
Ignatjs <ignatdzenkaev@gmail.com>
2022-03-21 22:17:06 +0300

    bot

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-var number = Match.floor(10*Math.random()) + 1; //a number is generated from 10 to 1
+var number = Math.floor(10*Math.random()) + 1; //a number is generated from 10 to 1
 console.log(number);
 
-document.getElementById(check).onlick = function (){
-    var username = document.getElementById(mynume).value;
-    username = parseInt(username);
+document.getElementById('check').onclick = function (){
+    var usernume = document.getElementById('mynume').value;
+    usernume = parseInt(usernume);
     var out = document.getElementById('out');
 
-    if (username == number){
+    if (usernume == number){
        out.innerHTML = 'you guessed';
     }
-
+    else if (usernume > number){
+        out.innerHTML = "Missed";
+    } 
+    else if (usernume<number){
+        out.innerHTML = "not at all"
+        
+    
+    }
 }
+
